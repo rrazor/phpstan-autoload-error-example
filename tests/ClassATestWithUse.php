eea55d648d16e2bf5d9cb31a66e108b28e14f05e
Matt Gray <rrazor@gmail.com>
2021-04-10 12:49:57 -0500

    Also use the extends

--- tests/ClassATestWithUse.php
+++ tests/ClassATestWithUse.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Example\Tests;
 
+use Example\ClassA;
 use Example\ClassAHelperTrait;
 
 class ClassATestWithUse
 {
     public function test(): void
     {
-        $anonStub = new class() extends \Example\ClassA
+        $anonStub = new class() extends ClassA
         {
             use ClassAHelperTrait;
         };
